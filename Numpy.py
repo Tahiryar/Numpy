478e9fe46704c05e7e8db0ce24335595a168caf1
Tahiryar <tahiryar.251k@gmail.com>
2025-03-08 22:12:12 +0500

    Numpy with img load

--- Numpy.py
+++ Numpy.py
@@ -1,4 +1,5 @@
 import numpy as np
+from PIL import Image
 
 arr = np.array([1, 2, 3, 4, 5])
 print("NumPy Array:", arr)
@@ -6,3 +7,11 @@ print("NumPy Array:", arr)
 
 matrix = np.array([[1, 2], [3, 4]])
 print("Matrix:\n", matrix)
+
+# Image Load Karna NumPy se
+
+# Image ko Load Karna
+img = Image.open("image.jpg")
+img_array = np.array(img)
+
+print("Image Shape:", img_array.shape)  # Height x Width x Channels
