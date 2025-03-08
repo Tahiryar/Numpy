400f9d020410bcaecb342dda7f6c6eeccbf2b4ef
Tahiryar <tahiryar.251k@gmail.com>
2025-03-08 22:14:30 +0500

    Simple Neural Network ka Input Processing

--- Numpy.py
+++ Numpy.py
@@ -15,3 +15,16 @@ img = Image.open("image.jpg")
 img_array = np.array(img)
 
 print("Image Shape:", img_array.shape)  # Height x Width x Channels
+
+
+# Input Features (5 Features)
+X = np.random.rand(10, 5)  # 10 Samples, 5 Features Each
+
+# Weights aur Bias
+W = np.random.rand(5, 3)  # 5 Inputs se 3 Neurons ko connect kar raha hai
+B = np.random.rand(3)
+
+# Neural Network ka Forward Pass
+output = np.dot(X, W) + B
+
+print("Neural Network Output:\n", output)
